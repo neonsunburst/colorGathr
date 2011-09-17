1f394917165c2c6d511c71fe91b6d6e4d5e11b80
dylanbathurst <dylanbathurst@gmail.com>
2011-09-17 14:26:27 -0700

    added etsy integration

--- app.js
+++ app.js
@@ -6,7 +6,9 @@
 var express = require('express'),
     fs      = require('fs'),
     mu      = require('mu2')
-    util    = require('util');
+    util    = require('util'),
+    http    = require('http'),
+    couch   = require('couch-client');
 
 var app = module.exports = express.createServer();
 
@@ -32,27 +34,64 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', function(req, res){
+  
+  // Couchdb stuff
+  // var colors = couch('http://dylanbathurst:macosx42@dylan.couchone.com:5984/colorgathr');
 
-  var view = view || {};
+  // colors.get('56b9b01098e0ca32d67a8dbc54000bda', function(err, doc) {
+  //   if (err) throw err;
 
-  mu.root = __dirname + '/views/';
+  //   console.log(doc);
+  // });
+  var buffer = '';
+  var options = {
+    host: 'openapi.etsy.com',
+    port: 80,
+    // path: '/v2/listings/active?limit=5&color=000000&api_key=7e55a71jhqrwd9w7fa8ohdzu'
+    path: '/v2/listings/active?limit=5&color=[0,0,0]&includes=MainImage&api_key=7e55a71jhqrwd9w7fa8ohdzu'
+  };
 
-  res.writeHead(200, {
-    'content-type': 'text/html'
-  });
+  http.get(options, function(response) {
+    res.writeHead(200, {
+      'content-type': 'text/html'
+    });
 
-  mu.compile('index.html', function (err, parsed) {
-    if (err) {
-      exports.error(res, 404, 'error compiling template');
-      return;
-    };
+    response.on('data', function(chunk) {
+      buffer += chunk;
+    }).on('end', function () {
+      var view = JSON.parse(buffer);
 
-    var readableStream = mu.render('index.html', view);
+      view.username = randomString();
 
-    util.pump(readableStream, res, function (err) {
-      throw err;
+      mu.root = __dirname + '/views/';
+
+      mu.compile('index.html', function (err, parsed) {
+        if (err) {
+          throw error;
+        };
+
+        var readableStream = mu.render('index.html', view);
+
+        util.pump(readableStream, res, function (err) {
+          throw err;
+        });
+      });
+
+    }).on('error', function(e) {
+      console.log('AHHHHHHH');  
     });
   });
+
+  function randomString() {
+    var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+    var string_length = 6;
+    var randomstring = '';
+    for (var i=0; i<string_length; i++) {
+      var rnum = Math.floor(Math.random() * chars.length);
+      randomstring += chars.substring(rnum,rnum+1);
+    }
+    return randomstring;
+  }
 
 });
 
